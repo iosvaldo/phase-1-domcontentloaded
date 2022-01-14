f17272362039bbbb5a4f198c13bc1aaa8a053c7a
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-14 15:38:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+//Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+  let text = document.getElementById('text');
+  text.textContent = 'This is really cool!';
+  console.log('The DOM has loaded and parsed')
+});
+console.log('This parsed beforehand.')
